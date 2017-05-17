3789bf29886abdbefd02dbcd3085f643abce6ab9
Kelly McBride <kemcbride28@gmail.com>
2017-05-17 13:15:57 -0400

    shortest_distance.py

--- virtual_env/shortest_distance.py
+++ virtual_env/shortest_distance.py
@@ -1,5 +1,3 @@
-import os
-
 file = open("scaffold_distances", "r")
 
 file.readline()
@@ -10,17 +8,17 @@ while line != "":
 		name = line
 		line = file.readline().strip()
 	else:
-		current = line.split("      ")
+		current = line.split()
 		id = current[0]
 		result = current
 		while line.startswith(id):
-			if int(current[2]) < int(result[2]):
+			if float(current[2]) < float(result[2]):
 				result = current
 			line = file.readline().strip()
-			current = line.split("      ")
+			current = line.split()
 		results.append([name, result])
 for x in results:
 	print (x[0])
-	print ("{0}	{1}	{2}".format(x[1][0], x[1][1], x[1][2]))
+	print ("{0}\t{1}\t{2}".format(x[1][0], x[1][1], x[1][2]))
 file.close()
 
