5409fd19bebc7f668068969c099199f989d9c3e9
Kelly McBride <kemcbride28@gmail.com>
2017-05-25 17:27:43 -0400

    Adding a fucntion that I think will be useful to determining files that contain specific motif regexes

--- virtual_env/ppm_motif_filter.py
+++ virtual_env/ppm_motif_filter.py
@@ -1,5 +1,7 @@
-INPUT_PATH = /Vagabundo/monica/temp/faa-Complete/
-DATA_PATH = /Vagabundo/monica/temp/70-CUTGA-faa-Complete/
+import os, re
+
+INPUT_PATH = '/Vagabundo/monica/temp/faa-Complete/'
+DATA_PATH = '/Vagabundo/monica/temp/70-CUTGA-faa-Complete/'
 #Check filenames in 70-CUTGA (.out) and scan for those filenames in faa-Complete(.faa)
 #Read filenames in faa-Complete (amino acid sequences) to scan for conserved motif (EDKXXXXXNS)
 #The XXXXX is a variable region where X can be any letter
@@ -14,9 +16,27 @@ def multi_match(target, patterns):
 
 def multi_search(target,patterns):
     """Return the first position in target where any of the strings in patterns is found"""
-    return min([target .find(pattern)
+    return min([target.find(pattern)
                 for pattern in patterns
-                if target.find(pattern >= 0])
+                if target.find(pattern >= 0)])
      
 multi_match(sequence, ('EDK,\D, \D, \D, \D, \D, NS'))
 multi_search(sequence, ('EDK,\D, \D, \D, \D, \D, NS'))
+
+
+################################################################################
+# So those are great, for learning and stuff
+# But really you'd have to debug them and fix them to get them to work...
+# I think what we want is the same gist as this:
+# this is not tested so it'll need to be fixed up probably
+
+# Where we want the file_path to be like, INPUT_PATH+file_name
+# And we want teh regex to be something like:
+# 'EDK\w{3,7}NS'
+def file_has_motif(file_path, motif_regex):
+    file_contents = open(file_path, 'r').read()
+    has_motif = bool(re.search(file_contents), motif_regex)
+    return has_motif
+
+
+################################################################################
--- virtual_env/util.py
+++ virtual_env/util.py
@@ -18,6 +18,9 @@ MULTIPROCESSING_FACTOR = 100 # We'll run 100 per batch
 # You could pass in a lambda/function to cond and have that also filter out
 # false results from idir (ie. is file in idir also in sort_matches both.txt)
 def files_remaining(idir, odir, cond=lambda x: true):
+    """Pass None if you want no filtering to happen...
+    Although the default value is also a pass-thru
+    """
     files = [f for f in os.listdir(idir) if f not in os.listdir(odir)]
     if cond is not None:
         files = [f for f in files if cond(f)]
