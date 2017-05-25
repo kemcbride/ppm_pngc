1eb76770a1cb530a415d98ae593a6d68ec678737
Kelly <kemcbride28@gmail.com>
2017-05-25 16:34:19 -0400

    Update ppm_motif_filter.py
    moinka's comments

--- virtual_env/ppm_motif_filter.py
+++ virtual_env/ppm_motif_filter.py
@@ -1,4 +1,10 @@
-DATA_PATA = /Vagabundo/monica/temp/faa-Complete/
+INPUT_PATH = /Vagabundo/monica/temp/faa-Complete/
+DATA_PATH = /Vagabundo/monica/temp/70-CUTGA-faa-Complete/
+#Check filenames in 70-CUTGA (.out) and scan for those filenames in faa-Complete(.faa)
+#Read filenames in faa-Complete (amino acid sequences) to scan for conserved motif (EDKXXXXXNS)
+#The XXXXX is a variable region where X can be any letter
+#The variable region itself can vary in number of X - scan for 3 - 7X
+#Output should be all of the filenames that have this motif from faa-Complete
 
 def multi_match(target, patterns):
     """Return True if target begins with any of the strings in patterns"""
