b92cab5d7bfb4c05c3e8ecabdd8322b88bbeecf3
monicapapinski <monicapapinski@hotmail.com>
2017-05-30 13:52:38 -0400

    Create filter_redundant.py

---
+++ virtual_env/filter_redundant.py
@@ -0,0 +1,13 @@
+#WRITING A CODE TO FILTER OUT REDUNDANT GENOMES, AND TO NARROW RESULTS DOWN TO THOSE THAT HAVE P$
+
+import os, re
+
+LIST = '/Vagabundo/monica/notes/complete_list'
+PPM = '/Vagabundo/monica/notes/virtual_env/ppm_motif_complete_matches'
+OUTPUT = '/Vagabundo/monica/notes/virtual_env/shortest_complete_distance'
+
+#OUTPUT FORMAT is GCF_000005225 for LIST_PATH and GCF_000005225.out for PPM_PATH
+
+if __name__ == '__main__':
+    with open(LIST, "r") as ins:
+
