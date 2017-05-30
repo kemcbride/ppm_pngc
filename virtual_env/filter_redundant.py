735fa21d8ce7f99142de0448b96361b2961a0fb4
monicapapinski <monicapapinski@hotmail.com>
2017-05-30 14:48:53 -0400

    Update filter_redundant.py

--- virtual_env/filter_redundant.py
+++ virtual_env/filter_redundant.py
@@ -19,7 +19,8 @@ if __name__ == '__main__':
                 fname = line.split()[-1]
                 ppm_name = line.split()[-1]
                 nonredundant_name = ppm_name.split('.')[0]
-                if motif_name in motif_list and nonredundant_name in nonredundant_list:
+                if ppm_name in ppm_motif_list and nonredundant_name in nonredundant_list:
                    print()
+
 
 
